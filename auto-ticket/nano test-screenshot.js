fe77a0db85b5ee28551d1050ab3a4b83ae6df4fd
rewritechun <47963356+rewritechun@users.noreply.github.com>
2025-06-08 15:35:17 +0800

    Delete auto-ticket/nano test-screenshot.js

--- auto-ticket/nano test-screenshot.js
+++
@@ -1,9 +0,0 @@
-const { chromium } = require('playwright');
-
-(async () => {
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
-  await page.goto('https://www.baidu.com', { waitUntil: 'networkidle' });
-  await page.screenshot({ path: '/var/www/html/screenshots/screenshot.png', fullPage: true });
-  await browser.close();
-})();
